832bac45d352094c28586e2f9963144816d3ad94
Ian Firkin <ian.firkin@gisinc.com>
2016-10-24 19:36:12 -0400

    Adds methods for http requests and output

--- getignore.go
+++ getignore.go
@@ -1,4 +1,12 @@
-package getignore
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
 type ignoreFetcher struct {
 	baseUrl string
@@ -14,4 +22,51 @@ func (fetcher *ignoreFetcher) NamesToUrls(namesChannel chan string, urlsChannel 
 
 func (fetcher *ignoreFetcher) NameToUrl(name string) string {
 	return fetcher.baseUrl + "/" + name + ".gitignore"
+}
+
+func FetchIgnoreFiles(urlsChannel chan string, contentChannel chan string) error {
+	var err error = nil
+	for url := range urlsChannel {
+		response, err := http.Get(url)
+		if err != nil {
+			close(contentChannel)
+			return err
+		}
+		content, err := getContent(response.Body)
+		if err != nil {
+			close(contentChannel)
+			return err
+		}
+		contentChannel <- content
+	}
+	close(contentChannel)
+	return err
+}
+
+func getContent(body io.ReadCloser) (string, error) {
+	var err error = nil
+	output := ""
+	scanner := bufio.NewScanner(body)
+	for scanner.Scan() {
+		output = output + fmt.Sprintln(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+	return output, err
+}
+
+func writeContent(writer bufio.Writer, contentChannel chan string) error {
+	var err error = nil
+	for content := range contentChannel {
+		_, err := writer.WriteString(content)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "writing output:", err)
+			return err
+		}
+	}
+	return err
+}
+
+func main() {
 }
--- getignore_test.go
+++ getignore_test.go
@@ -1,4 +1,4 @@
-package getignore
+package main
 
 import (
 	"reflect"
