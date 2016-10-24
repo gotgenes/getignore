3a06edf51b9819b2e573847a307e2f2f28e02c87
Chris Lasher <chris.lasher@gmail.com>
2016-10-23 23:31:05 -0700

    Implements NamesToUrls via channels

--- ignoreme.go
+++ ignoreme.go
@@ -4,6 +4,14 @@ type ignoreFetcher struct {
 	baseUrl string
 }
 
-func (fetcher ignoreFetcher) NameToUrl(name string) string {
+func (fetcher *ignoreFetcher) NamesToUrls(namesChannel chan string, urlsChannel chan string) {
+	for name := range namesChannel {
+		url := fetcher.NameToUrl(name)
+		urlsChannel <- url
+	}
+	close(urlsChannel)
+}
+
+func (fetcher *ignoreFetcher) NameToUrl(name string) string {
 	return fetcher.baseUrl + "/" + name + ".gitignore"
 }
--- ignoreme_test.go
+++ ignoreme_test.go
@@ -1,6 +1,9 @@
 package ignoreme
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 var error_template string = "Got %q, expected %q"
 
@@ -11,6 +14,38 @@ func TestIgnoreFetcher(t *testing.T) {
 	if gotUrl != baseUrl {
 		t.Errorf(error_template, gotUrl, baseUrl)
 	}
+}
+
+func TestNamesToUrls(t *testing.T) {
+	fetcher := ignoreFetcher{baseUrl: "https://github.com/github/gitignore"}
+	names := []string{"Go", "Python"}
+	namesChannel := make(chan string)
+	go arrayToChannel(namesChannel, names)
+	urlsChannel := make(chan string)
+	go fetcher.NamesToUrls(namesChannel, urlsChannel)
+	urls := channelToArray(urlsChannel)
+	expectedUrls := []string{
+		"https://github.com/github/gitignore/Go.gitignore",
+		"https://github.com/github/gitignore/Python.gitignore",
+	}
+	if !reflect.DeepEqual(urls, expectedUrls) {
+		t.Errorf(error_template, urls, expectedUrls)
+	}
+}
+
+func arrayToChannel(c chan string, a []string) {
+	for _, v := range a {
+		c <- v
+	}
+	close(c)
+}
+
+func channelToArray(c chan string) []string {
+	var a []string
+	for v := range c {
+		a = append(a, v)
+	}
+	return a
 }
 
 func TestNameToUrl(t *testing.T) {
