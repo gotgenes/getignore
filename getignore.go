1e1e5c78583f7a1d7c507f108f6303a7eeeca492
Ian Firkin <ian.firkin@gisinc.com>
2016-10-25 13:18:04 -0400

    Extracts method to get names from arguments
    * Extracts a method to build a full list of names
    * Renames method to add name list to channel

--- getignore.go
+++ getignore.go
@@ -25,11 +25,23 @@ func (fetcher *ignoreFetcher) NameToUrl(name string) string {
 	return fetcher.baseUrl + "/" + name + ".gitignore"
 }
 
-func getNames(names []string, namesChannel chan string) {
+func addNamesToChannel(names []string, namesChannel chan string) {
 	for _, v := range names {
 		namesChannel <- v
 	}
 	close(namesChannel)
+}
+
+func getNamesFromArguments() []string {
+	filePointer := flag.String("file", "", "Path to file of names")
+	flag.Parse()
+	names := flag.Args()
+
+	if *filePointer != "" {
+		namesFile, _ := os.Open(*filePointer)
+		names = append(names, getNamesFromFile(namesFile)...)
+	}
+	return names
 }
 
 func getNamesFromFile(f *os.File) []string {
@@ -87,20 +99,11 @@ func writeContent(f *os.File, contentChannel chan string) error {
 
 func main() {
 	fetcher := ignoreFetcher{baseUrl: "https://raw.githubusercontent.com/github/gitignore/master"}
-
-	filePointer := flag.String("file", "", "Path to file of names")
-	flag.Parse()
-	names := flag.Args()
-
-	if *filePointer != "" {
-		namesFile, _ := os.Open(*filePointer)
-		names = append(names, getNamesFromFile(namesFile)...)
-	}
-
+	names := getNamesFromArguments()
 	namesChannel := make(chan string)
 	urlsChannel := make(chan string)
 	contentChannel := make(chan string)
-	go getNames(names, namesChannel)
+	go addNamesToChannel(names, namesChannel)
 	go fetcher.NamesToUrls(namesChannel, urlsChannel)
 	go FetchIgnoreFiles(urlsChannel, contentChannel)
 	f, _ := os.Create(".gitignore")
