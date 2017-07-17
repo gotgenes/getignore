cbc81cadc33f32abd3bbbb87581a93e418b01bb2
Chris Lasher <chris.lasher@gmail.com>
2017-07-16 21:39:25 -0700

    Release 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## 1.0.0 - 2017-07-16
 
 ### Changed
 
--- getignore.go
+++ getignore.go
@@ -16,7 +16,7 @@ import (
 )
 
 // Version is the version of getignore
-const Version string = "0.3.0"
+const Version string = "1.0.0"
 
 func main() {
 	log.SetFlags(0)
